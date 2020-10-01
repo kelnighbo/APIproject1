e722c02f18adc60f4af50079e1810c7e500f5b4e
Kelly Marie Nighbert <Kelly@Kellys-MacBook-Pro.local>
2020-09-30 20:08:08 -0400

    second

--- lorem.js
+++ lorem.js
@@ -1,5 +1,6 @@
 baseURL = "https://picsum.photos";
 let url;
+const imgsArray = ['https://picsum.photos/id/10/200/300', 'https://picsum.photos/id/1006/200/300', 'https://picsum.photos/id/1018/200/300', 'https://picsum.photos/id/1020/200/300']
 
 console.log('hello')
 
@@ -12,7 +13,16 @@ function fetchImages() {
 }
 
 function displayImages() {
-    document.getElementById
+    var imgs = document.getElementById("imgs")
+    let container = document.getElementById("container")
+    for (let i = 0; i < imgsArray.length; i++) {
+        console.log(imgsArray[i]);
+        let img = document.createElement("img")
+        img.src = imgsArray[i]
+        container.appendChild(img)
+      }
 };
 
-fetchImages();
+//fetchImages();
+displayImages();
+
